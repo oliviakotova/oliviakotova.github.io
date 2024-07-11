faa30db5869334d290c02f48712af2ccebc8a3b6
Olivia <56949536+oliviakotova@users.noreply.github.com>
2024-07-11 23:21:23 +1000

    make it better

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b978c74631a6d63a"; }
+/******/ 		__webpack_require__.h = function() { return "2e2adbc9e120706a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
