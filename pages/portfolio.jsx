e04fc7584c854545f4faadb4a2f1ecc193b225e6
Olivia <56949536+oliviakotova@users.noreply.github.com>
2025-02-22 00:00:02 +1000

    portf. added

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -6,16 +6,7 @@ import Link from "next/link";
 import PhotoSlider from "../components/PhotoSlider";
 
 const portfolio = () => {
-  const images = [
-    "/assets/carepulse_slider/photo1.jpg",
-    "/assets/carepulse_slider/photo1_cp.png",
-    "/assets/carepulse_slider/photo2_cp.png",
-    "/assets/carepulse_slider/photo3_cp.png",
-    "/assets/carepulse_slider/photo4_cp.png",
-    "/assets/carepulse_slider/photo5_cp.png",
-    "/assets/carepulse_slider/photo6_cp.png",
-    "/assets/carepulse_slider/photo7_cp.png",
-  ];
+  const images = ["/assets/projects/portfolio.png"];
 
   return (
     <div className="w-full">
@@ -25,12 +16,13 @@ const portfolio = () => {
 
       <div className="max-w-[1240px] mx-auto p-6 grid md:grid-cols-5 gap-8 py-24">
         {/* Left Column for Text Content */}
-        <div className="col-span-4 flex flex-wrap gap-8">
-          <h2>
-            Patient Management System with Next.js | Twilio, TypeScript,
-            TailwindCSS{" "}
-          </h2>
-          <Link href="https://github.com/oliviakotova/carepulse" passHref>
+        <div className="col-span-4  ">
+          <p>In progress</p>
+          <h2>Personal Website</h2>
+          <Link
+            href="https://github.com/oliviakotova/oliviakotova.github.io.git"
+            passHref
+          >
             <a
               target="_blank"
               rel="noopener noreferrer"
@@ -39,66 +31,6 @@ const portfolio = () => {
               ⚙️ GitHub
             </a>
           </Link>
-
-          <Link href="https://carepulse-five-rho.vercel.app" passHref>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              className="underline cursor-pointer px-8 mt-4 hover:scale-110 ease-in duration-100 projects-link"
-            >
-              🚀 Visit App
-            </a>
-          </Link>
-
-          <div>
-            <h4 className="py-4">Overview </h4>
-            <p>
-              CarePulse - a healthcare booking application that allows patients
-              to easily register, book, and manage their appointments with
-              doctors, featuring administrative tools for scheduling,
-              confirming, and canceling appointments, along with SMS
-              notifications, all built using Next.js.
-            </p>
-          </div>
-
-          <div>
-            <h4 className="py-4 mt-4 mr-8">Features</h4>
-            <p>
-              👉 Users can sign up and create a personal profile as a patient.
-            </p>
-            <p>
-              👉 Patients can schedule appointments with doctors at their
-              convenience and can book multiple appointments.
-            </p>
-            <p>
-              👉 Administrators can efficiently view and handle all scheduled
-              appointments.
-            </p>
-            <p>
-              👉 Admins can confirm and set appointment times to ensure they are
-              properly scheduled.
-            </p>
-            <p>
-              👉 Administrators have the ability to cancel any appointment if
-              needed.
-            </p>
-            <p>
-              👉 Patients receive SMS notifications to confirm their appointment
-              details using Twilio.
-            </p>
-            <p>
-              👉 The application works seamlessly on all device types and screen
-              sizes.
-            </p>
-            <p>
-              👉 Users can upload and store files securely within the app using
-              Appwrite storage services.
-            </p>
-            <p>
-              👉 The application uses Sentry to monitor and track its
-              performance and detect any errors.
-            </p>
-          </div>
         </div>
 
         {/* Right Column for Tech Stack */}
@@ -110,42 +42,16 @@ const portfolio = () => {
                 <RiRadioButtonFill className="pr-1" /> Next.js
               </p>
               <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> Appwrite
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> Typescript
+                <RiRadioButtonFill className="pr-1" /> Node.js
               </p>
               <p className="text-gray-600 py-2 flex items-center">
                 <RiRadioButtonFill className="pr-1" /> TailwindCSS
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> ShadCN
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> Twilio
               </p>
             </div>
           </div>
         </div>
       </div>
-      {/* Video Section */}
-      <div className=" max-w-[1240px] mx-auto p-6 ">
-        <h4 className=" mb-6"> Booking part video</h4>
-        <video
-          controls
-          className="w-full rounded-lg shadow-lg"
-          poster="/assets/carepulse_slider/photo5_cp.png" // Thumbnail for the video
-        >
-          {/* Video Source */}
-          <source
-            src="/assets/carepulse_slider/appoint.mov"
-            type="video/quicktime"
-          />
-          {/* Provide MP4 fallback for wider compatibility */}
-          <source src="/assets/carepulse_slider/appoint.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
+
       <div className="max-w-[1240px] mx-auto p-6 flex justify-between items-center">
         <Link href="/carepulse_admin">
           <p className="underline cursor-pointer py-8 mt-6 hover:scale-110 ease-in duration-100">
